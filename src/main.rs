71ea00a2fb92b4e3eef0dfe1f473c32c117aec87
PaologGithub <PaologGithub>
2026-05-16 19:55:21 +0200

    Included containerd

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use containerd_client::{
+    connect,
+    with_namespace,
+    services::v1::{
+        containers_client::ContainersClient,
+        version_client::VersionClient
+    }
+};
+
+#[tokio::main]
+async fn main() {
+    let channel = connect("/run/containerd/containerd.sock").await.unwrap();
+
+    let mut client = VersionClient::new(channel);
+    let resp = client.version(()).await.unwrap();
+
+    println!("Response: {:?}", resp.get_ref());
 }
